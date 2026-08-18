c6f7b9c4e3aa4b4dfe216344ae05f9706c8a6efd
Guilherme Augusto P. de A. e Silva <3guilugia3@gmail.com>
2026-08-18 17:42:03 +0000

    CheckMate_Logic_Implemented

--- src/application/UI.java
+++ src/application/UI.java
@@ -60,11 +60,21 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
-        System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
 
-        if (chessMatch.getCheck()) {
+        if (!chessMatch.getCheckMate()) {
+
+            System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
+
+            if (chessMatch.getCheck()) {
             System.out.println("Check!");
+            }
         }
+        else {
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+        }
+
+        
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
